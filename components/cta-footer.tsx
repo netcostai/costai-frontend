0bda45e0d4e0c095db5d08da2138bae9a712b9ad
netcostai <morrisjeffrey73@gmail.com>
2026-07-12 14:34:02 -0700

    Create cta-footer.tsx

---
+++ components/cta-footer.tsx
@@ -0,0 +1,7 @@
+export function CtaFooter() {
+  return (
+    <footer className="py-10 text-center bg-black text-white">
+      <button className="bg-white text-black px-6 py-2 rounded font-bold">Claim 14-Day Free Pilot</button>
+    </footer>
+  )
+}
