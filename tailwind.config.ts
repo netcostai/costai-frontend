382e9f16a82edab4a2ebba1e750df564e1d59fbb
netcostai <morrisjeffrey73@gmail.com>
2026-07-12 19:54:44 -0700

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#000000",
+        foreground: "#ffffff",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config;
