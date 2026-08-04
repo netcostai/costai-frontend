ecefb3c66291cf8ae6ed1dd14aae8f7a59970a04
netcostai <morrisjeffrey73@gmail.com>
2026-08-03 21:24:28 -0700

    Tightened top spacing slightly:

--- components/features.tsx
+++ components/features.tsx
@@ -23,7 +23,7 @@ const features = [
 
 export function Features() {
   return (
-    <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-20">
+    <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-16">
       <div className="grid sm:grid-cols-3 gap-6">
         {features.map(({ icon: Icon, title, description, color }) => (
           <div
