87732bd2e4e2dab3bda17ef61e1937e9b426b3b7
netcostai <morrisjeffrey73@gmail.com>
2026-08-03 21:08:31 -0700

    update page for providers

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/navbar";
 import { Hero } from "@/components/hero";
+import { SupportedProviders } from "@/components/supported-providers";
 import { Features } from "@/components/features";
 
 export default function Page() {
@@ -7,6 +8,7 @@ export default function Page() {
     <>
       <Navbar />
       <Hero />
+      <SupportedProviders />
       <Features />
     </>
   );
